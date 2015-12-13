8dbb17446f70f5e6f74f8f72bd1590bbf6a1c21e
huseyinbiyik <huseyinbiyik@hotmail.com>
2015-12-13 18:55:04 +0200

    fix weplayer domain

--- lib/scrapers/weplayer.py
+++ lib/scrapers/weplayer.py
@@ -6,8 +6,9 @@ import urllib
 encoding="utf-8"
 
 def run(id,referer,width="480",height="270"):
+	print id
 	wurl="http://weplayer.pw/stream.php?id=%s"%id
 	page=bt.get_page(wurl,encoding,referer=referer)
-	id=re.findall("<script type='text/javascript'>id='(.*?)'.*?src='http://deltatv.pw/player.js'",page)
+	id=re.findall("<script type='text/javascript'>id='(.*?)'.*?src='http://deltatv.*?/player.js'",page)
 	import deltatv
 	return deltatv.run(id[0],wurl)
